05fe1c7cb3e631de821b5949d878fc250a518824
ailihui <1993ailihui@gmail.com>
2025-12-19 13:47:11 +0800

    Character input and output functions
    this code nothing i wang to say ,because its so easy and simply

---
+++ 字符输入输出函数.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	//char ch = 'A';
+	////printf("%c", ch);
+	//putchar(ch);
+	//putchar('\n');
+	//putchar('2');
+	//putchar('x');
+	//putchar('\n');
+
+	//还有getchar函数，可以直接获取输入值
+	char ch;
+
+	ch = getchar();
+	putchar(ch);
+	putchar('\n');
+
+
+	return 0;
+
+}
